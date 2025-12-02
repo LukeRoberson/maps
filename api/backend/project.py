a10cdac8c356bd55c30d8aa0cda106513007e2d6
lrobertson <luke.robertson@networkdirection.net>
2025-12-02 21:11:54 +1100

    Removed last change. updated dockerfile with an ENV for python

--- api/backend/project.py
+++ api/backend/project.py
@@ -39,10 +39,6 @@ from typing import (
 from flask import current_app
 
 # Local imports
-import sys
-import os
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
 from database import (
     DatabaseContext,
     DatabaseManager
