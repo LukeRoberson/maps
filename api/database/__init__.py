4024a317870ebd90b230ddd7df883432afb16a8d
lrobertson <luke.robertson@networkdirection.net>
2026-03-18 12:08:34 +1100

    adjusted schema patch

--- api/database/__init__.py
+++ api/database/__init__.py
@@ -4,10 +4,8 @@ Database initialization and management.
 
 from .database import DatabaseContext
 from .database import DatabaseManager
-from .migrations import run_migrations
 
 __all__ = [
     'DatabaseContext',
     'DatabaseManager',
-    'run_migrations'
 ]
